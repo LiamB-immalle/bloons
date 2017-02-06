3854ab387def7bb271fe5a20e658da0221b973d1
liam.brangers <liam.brangers@B209-14>
2017-02-06 11:27:40 +0100

    Werkt nog hallelujah

---
+++ Bloons/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bloons
+{
+    public class Balloon
+    {
+        private int x = 50;
+        private int y = 50;
+        private int d = 20;
+        private Ellipse elli;
+
+        public Balloon()
+        {
+            CreateEllipse();
+            UpdateEllipse();
+        }
+        public void MoveRight(int xStep)
+        {
+            x = x + xStep;
+            UpdateEllipse();
+        }
+        public void ChangeSize(int change)
+        {
+            d = d + change;
+            UpdateEllipse();
+        }
+        public void DisplayOn(Canvas canvas)
+        {
+            canvas.Children.Add(elli);
+        }
+        private void CreateEllipse()
+        {
+            elli = new Ellipse();
+            elli.Stroke = new SolidColorBrush(Colors.Red);
+            elli.StrokeThickness = 2;
+        }
+        private void UpdateEllipse()
+        {
+            elli.Margin = new Thickness(x, y, 0, 0);
+            elli.Width = d;
+            elli.Height = d;
+        }
+    }
+}
--- Bloons/MainWindow.xaml.cs
+++ Bloons/MainWindow.xaml.cs
@@ -15,14 +15,26 @@ using System.Windows.Shapes;
 
 namespace Bloons
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent(); ;
+            b.DisplayOn(canvas);
+        }
+        Balloon b = new Balloon();
+        Balloon c = new Balloon();
+        Balloon d = new Balloon();
+        Balloon e = new Balloon();
+
+        private void move_Click(object sender, RoutedEventArgs e)
+        {
+            b.MoveRight(5);
+        }
+
+        private void grow_Click(object sender, RoutedEventArgs e)
+        {
+            b.ChangeSize(5);
         }
     }
-}
+}       
